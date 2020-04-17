e0662e6a3c34504f5fa19fc4849a085b7c2fe006
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-18 00:12:51 +0200

    Script for Transparent Navbar

--- js/main.js
+++ js/main.js
@@ -18,3 +18,13 @@ $('#navbar a, .btn').on('click', function (event) {
     $('html,body').animate({ scrollTop: $(hash).offset().top - 100 }, 800);
   }
 });
+
+// Sticky menu backgroud
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
