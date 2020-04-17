0e70271048c8f71ba7f5d3f66b2d4cf136aa3546
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-18 00:07:36 +0200

    smooth scroll using jquery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 function initMap() {
-  map = new google.maps.Map(document.querySelector('.map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 14,
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 4,
+    center: uluru,
   });
-  const marker = new google.maps.Marker({
-    position: { lat: -34.397, lng: 150.644 },
-    map: map,
-  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
 }
+// smooth scrolling
+
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html,body').animate({ scrollTop: $(hash).offset().top - 100 }, 800);
+  }
+});
